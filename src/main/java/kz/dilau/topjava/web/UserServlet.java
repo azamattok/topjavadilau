ad53a36a386fc7b6a09882f34e144e3d79350f47
azamat <tokabaev.azamat@gmail.com>
2020-08-25 16:06:14 +0600

    1_5_logging

--- src/main/java/kz/dilau/topjava/web/UserServlet.java
+++ src/main/java/kz/dilau/topjava/web/UserServlet.java
@@ -1,14 +1,18 @@
 package kz.dilau.topjava.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import static org.slf4j.LoggerFactory.getLogger;
 public class UserServlet extends HttpServlet {
+    private final static Logger log=getLogger(UserServlet.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("redirect to users");
         resp.sendRedirect("users.jsp");
     }
 }
